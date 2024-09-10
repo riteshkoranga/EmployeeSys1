ddd5a42394c142802cb7c5a5fa5790d3013f6301
riteshkoranga <riteshkoranga5@gmail.com>
2024-09-10 15:24:47 +0530

    Controller is used to handle static requests, removed reset button in emp_edit file

--- src/main/java/com/employee/backend/controller/WebController.java
+++ src/main/java/com/employee/backend/controller/WebController.java
@@ -1,10 +1,9 @@
 package com.employee.backend.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
+@Controller
 public class WebController {
 
     @GetMapping("/")
