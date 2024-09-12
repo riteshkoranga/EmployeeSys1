2a4e14d3c2b9224dca53c7dc496fe2d321cc8fdd
riteshkoranga <riteshkoranga5@gmail.com>
2024-09-12 16:33:12 +0530

    routing all links to index.html

--- src/main/java/com/employee/backend/controller/WebController.java
+++ src/main/java/com/employee/backend/controller/WebController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@CrossOrigin(origins = "https://employee-sys12-4b4a2f10c5a1.herokuapp.com")
 @Controller
 public class WebController {
 
