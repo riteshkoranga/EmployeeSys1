0c045a5ee660c8d1fb01a6fe53f5cf202246c605
riteshkoranga <riteshkoranga5@gmail.com>
2024-09-12 16:01:20 +0530

    added cors origin to wenController

--- src/main/java/com/employee/backend/controller/WebController.java
+++ src/main/java/com/employee/backend/controller/WebController.java
@@ -1,8 +1,10 @@
 package com.employee.backend.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@CrossOrigin(origins = "https://employee-sys12-4b4a2f10c5a1.herokuapp.com")
 @Controller
 public class WebController {
 
